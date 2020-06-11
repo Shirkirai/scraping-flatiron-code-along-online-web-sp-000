8bc63c7c834a1608cd05615e7bc4ab8c5457312b
Jung Lee <lee.jung.s95@gmail.com>
2020-06-11 05:03:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,6 +33,17 @@ class Scraper
     end
   end
 
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
 end
 Scraper.new.make_courses
 #Scraper.new.get_page
